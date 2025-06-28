69c967bf1604d92d6b88f89550aab1473350020e
Jack Chan <jackchw.uk@gmail.com>
2025-06-28 21:23:48 +0100

    Default detailed

--- components/Filters/LayoutSelect.tsx
+++ components/Filters/LayoutSelect.tsx
@@ -19,8 +19,8 @@ const LayoutSelect = ({ setLayout }: Props) => {
         className="block w-full border text-sm rounded-lg p-1 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
         onChange={(e) => setLayout(e.currentTarget.value as unknown as LibraryLayout)}
       >
-        <option>Thumbnail</option>
         <option>Detailed</option>
+        <option>Thumbnail</option>
         {/* <option>Minimal</option> */}
       </select>
     </div>
